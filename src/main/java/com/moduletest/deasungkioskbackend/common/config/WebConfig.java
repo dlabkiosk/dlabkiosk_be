d3a72e899ae6edbbd34a3d674c52f68df7cd61fe
jingyu-signaldecode <leejingyu@signal-decode.com>
2026-03-12 15:01:17 +0900

    CORS 설정 프론트 포트 추가

--- src/main/java/com/moduletest/deasungkioskbackend/common/config/WebConfig.java
+++ src/main/java/com/moduletest/deasungkioskbackend/common/config/WebConfig.java
@@ -20,7 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://localhost:3000",
                     "http://localhost:3001",
                     "http://localhost:3002",
-                    "http://localhost:3003")
+                    "http://localhost:3003",
+                    "http://localhost:5173",
+                    "http://localhost:5174",
+                    "http://localhost:5175")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
